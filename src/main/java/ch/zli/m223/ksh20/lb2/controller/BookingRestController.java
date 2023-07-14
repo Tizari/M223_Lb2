22c965b31f84efd3076367cea8f938b68cbc3ebc
Tizari <arifit@bzz.ch>
2023-07-14 09:23:29 +0200

    implemented BookingRestController /add, /edit and /show

--- src/main/java/ch/zli/m223/ksh20/lb2/controller/BookingRestController.java
+++ src/main/java/ch/zli/m223/ksh20/lb2/controller/BookingRestController.java
@@ -51,43 +51,73 @@ public class BookingRestController {
     }
 
     @GetMapping("/show/{id}")
-    Map<String, String> getBooking(@PathVariable Long id){
+    @ResponseBody
+    public Map<String, String> getBooking(@PathVariable Long id) {
         Booking booking = bookingService.getBookingById(id);
         AppUser user = userService.getUserById(booking.getUserId());
         HashMap<String, String> map = new HashMap<>();
         map.put("date", booking.getDate().toString());
         map.put("isFullDay", String.valueOf(booking.isFullDay()));
-        map.put("accepted", String.valueOf(booking.accepted()));
+        map.put("accepted", String.valueOf(booking.isAccepted()));
         map.put("user", user.getFirstName() + "" + user.getLastName());
         return map;
     }
 
-    @PutMapping("/{id}/update")
-    void update(@RequestBody BookingInputDto bookingInput, @PathVariable Long id) {
-        bookingService.updateBooking(id, LocalDate.parse(bookingInput.date), bookingInput.isFullDay, bookingInput.accepted);
+
+
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        Booking booking = bookingService.getBookingById(id);
+        model.addAttribute("booking", booking);
+        return "/editBooking";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateBooking(@PathVariable Long id, @ModelAttribute("booking") BookingInputDto bookingInput) {
+        bookingService.updateBooking(id, bookingInput.getDate(), bookingInput.isFullDay(), bookingInput.isAccepted());
+        return "redirect:/api/v1/bookings/show/" + id;
+    }
+
+
+
+
+    @GetMapping("/add")
+    public String showBookingForm(Model model, Principal principal) {
+        // Überprüfen, ob ein Benutzer angemeldet ist
+        String username = principal != null ? principal.getName() : null;
+        if (username == null) {
+            // Kein Benutzer angemeldet, Fehler anzeigen oder Weiterleitung zu einer Fehlerseite
+            return "error";
+        }
+
+        // Buchungsformular anzeigen
+        BookingImpl booking = new BookingImpl();
+        booking.setUserId(userService.getUserByEmail(username).get().getId().longValue());
+        model.addAttribute("booking", booking);
+        return "/createBooking";
     }
 
     @PostMapping("/add")
-    String addBooking(@RequestBody BookingInputDto bookingInput, Principal principal, RedirectAttributes redirectAttributes) {
-        if (principal == null) {
-            redirectAttributes.addFlashAttribute("error", "Benutzer ist nicht angemeldet");
-            return "redirect:/index";
+    public String addBooking(@ModelAttribute("booking") Booking booking) {
+        // Überprüfen, ob ein Benutzer angemeldet ist
+        String username = userService.getUserById(booking.getUserId()).getUserName();
+        if (username == null || username.isEmpty()) {
+            // Kein Benutzer angegeben, Fehler anzeigen oder Weiterleitung zu einer Fehlerseite
+            return "error";
         }
 
-        String username = principal.getName();
-        Optional<AppUser> user = userService.getUserByEmail(username);
-        if (user.isPresent()) {
-            try {
-                Long userId = user.get().getId();
-                BookingImpl booking = (BookingImpl) bookingService.addBooking(LocalDate.parse(bookingInput.date), bookingInput.isFullDay, bookingInput.accepted, userId);
-                bookingRepository.insertBooking(booking.getDate(), booking.isFullDay(), booking.accepted(),booking.getUserId());
-            } catch (Exception e) {
-                redirectAttributes.addFlashAttribute("error", "Fehler beim Hinzufügen der Buchung");
-            }
-        }
+        // Hier kannst du den entsprechenden Code für die POST-Anforderung implementieren
+        // Verarbeite die übergebenen Buchungsinformationen und füge sie der Datenbank hinzu
+
+        // Beispiel: Speichern der Buchung in der Datenbank
+        bookingRepository.insertBooking(booking.getDate(), booking.isFullDay(), booking.isAccepted(), booking.getUserId());
+
+
 
         return "redirect:/api/v1/bookings/all";
     }
+
 
 
 
